7d082c61f2ba05f105879bea836eb5dbb2ad5945
Alex Horton <alexhortonmusic@gmail.com>
2016-12-05 15:16:01 -0600

    basic frontpage layout

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ const makeBurger = () => {
   $('.navMenu').slicknav({
     appendTo: 'nav',
     label: ''
-
   })
 }
 
 makeBurger()
+
+$('.slicknav_btn').click(() => {
+  $('#navLogo').toggleClass('slickLogo')
+  console.log('Hi')
+})
