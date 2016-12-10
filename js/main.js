c93b4bde5f65abb81a9320353f8ee2f58a2fe748
Alex Horton <alexhortonmusic@gmail.com>
2016-12-10 15:53:22 -0600

    for sale page/draft complete

--- js/main.js
+++ js/main.js
@@ -12,3 +12,41 @@ $('.slicknav_btn').click(() => {
   $('#navLogo').toggleClass('slickLogo')
   console.log('Hi')
 })
+
+// for sale photo grid
+
+const getForSaleItems = () => {
+  return new Promise ((resolve, reject) => {
+    $.ajax({
+      url: 'js/for-sale.json'
+    }).done(function(data) {
+      resolve(data)
+    }).fail(function(xhr, status, err) {
+      reject(err)
+    })
+  })
+}
+
+const gridHTML = (arr) => {
+  arr.forEach(item => {
+    let photo = `
+    <div class='photo'>
+      <div class='photoWrap'>
+        <img src='${item.photo}'/>
+      </div>
+      <p class='itemName'>${item.name}</p>
+      <p class='itemPrice'>${item.price}</p>
+    </div>
+    `
+    $('.forSaleGrid').append(photo)
+  })
+}
+
+const buildPhotoGrid = () => {
+  getForSaleItems()
+  .then(({ items }) => {
+    gridHTML(items)
+  })
+}
+
+buildPhotoGrid()
